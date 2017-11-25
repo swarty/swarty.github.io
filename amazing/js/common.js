fc8bffc14473267b93cf2948330a8e9d909f78db
swarty_d <swarty16@gmail.com>
2017-11-25 08:22:09 +0200

    new proj

---
+++ amazing/js/common.js
@@ -0,0 +1,10 @@
+var navbar = document.getElementById("logo");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
