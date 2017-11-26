83e68068ad1dd1a9be7731188c209147809699cf
swarty_d <swarty16@gmail.com>
2017-11-26 17:00:28 +0200

    changes

--- amazing/js/common.js
+++ amazing/js/common.js
@@ -37,14 +37,11 @@ function showDivs(n) {
 
 // popup
 var modal = document.getElementById('myModal');
-
 // Get the button that opens the modal
 var btn = document.getElementById("btn_popup");
 var btn1 = document.getElementById("btn_popup1");
 var btn2 = document.getElementById("btn_popup2");
-
 var but = document.getElementsByClassName("close")[0];
-
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
     modal.style.display = "block";
@@ -55,12 +52,10 @@ btn1.onclick = function() {
 btn2.onclick = function() {
     modal.style.display = "block";
 }
-
 // When the user clicks on <span> (x), close the modal
 but.onclick = function() {
     modal.style.display = "none";
 }
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
@@ -70,7 +65,6 @@ window.onclick = function(event) {
 
 
 // accordion
-
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -86,3 +80,4 @@ for (i = 0; i < acc.length; i++) {
   }
 }
 
+// tabs
