318c379d3cb4f5eb9fcfc23458f1ff7ad420c851
swarty_d <swarty16@gmail.com>
2017-11-26 12:54:57 +0200

    prev

--- amazing/js/common.js
+++ amazing/js/common.js
@@ -33,3 +33,42 @@ function showDivs(n) {
 			x[slideIndex-1].style.display = "block";  
 			dots[slideIndex-1].className += " slider_select";
 }
+
+
+// popup
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("btn_popup");
+var btn1 = document.getElementById("btn_popup1");
+var btn2 = document.getElementById("btn_popup2");
+
+var but = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+btn1.onclick = function() {
+    modal.style.display = "block";
+}
+btn2.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+but.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+// smooth-scroll
+
+
