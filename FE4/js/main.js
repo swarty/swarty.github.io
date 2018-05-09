d6b56fa56cffae789f82997c272867932995dfa7
swarty_d <swarty16@gmail.com>
2018-05-09 20:08:14 +0300

    update sections

--- FE4/js/main.js
+++ FE4/js/main.js
@@ -27,4 +27,37 @@ if(document.querySelector('.s_course__cards'))
    let rellax = new Rellax('.rellax', {
       center: true
    });
+
+
+// soft svg logic
+function softLogic(){
+    $('.soft__part1').click( function(){
+        $('div[data-block]').slideUp();
+        $('div[data-block="3"]').slideDown();
+    })
+
+    $('.soft__part2').click( function(){
+        $('div[data-block]').slideUp();
+        $('div[data-block="1"]').slideDown();
+    })
+
+    $('.soft__part3').click( function(){
+        $('div[data-block]').slideUp();
+        $('div[data-block="2"]').slideDown();
+    })
+}
+
+
+// enquire for svg in section soft
+
+
+enquire.register("screen and (min-width: 768px)", {
+    match : function() {
+        softLogic();
+    },
+    unmatch : function() {
+        // if unmatch
+    }
+});
+
 })
