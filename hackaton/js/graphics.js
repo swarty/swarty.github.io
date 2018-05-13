c3e62a44287076d559d91a14fba484f8e424bae8
dmytro_ohorodnykov <swarty16@gmail.com>
2018-05-13 17:01:22 +0300

    update new

---
+++ hackaton/js/graphics.js
@@ -0,0 +1,15 @@
+
+
+/* ========== DOM ELEMENTS END ========== */
+
+/* ========== CHART UPDATE TYPE ============ */
+
+/* ========== CHART UPDATE TYPE END ============ */
+/* ========== CHART UPDATE NAME  ============ */
+
+/* ========== CHART UPDATE NAME END ============ */
+
+/* ============ LISTNER ============ */
+
+// elemInputChartName.addEventListener('blur', updateName, false);
+/* ========== LISTNER END ========== */
